6a46eba0bd01b319df3b1d306cb8ff71b7ffe430
Stefan Worzalla <stefan.worzalla@gmail.com>
2016-08-27 22:37:20 +0700

    onClick listeners
    implemented onClick listeners to change to the 4 category activities

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,11 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +28,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find Views for the different categories and set their onClick methods for the intents
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        if (numbers != null) {
+            numbers.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, NumbersActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        TextView colors = (TextView) findViewById(R.id.colors);
+        if (colors != null) {
+            colors.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        TextView family = (TextView) findViewById(R.id.family);
+        if (family != null) {
+            family.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+        if (phrases != null) {
+            phrases.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, PhrasesActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
+
 }

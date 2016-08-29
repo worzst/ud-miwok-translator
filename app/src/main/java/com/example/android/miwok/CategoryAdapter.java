81e5cd5b5c78ae502ba39524e325ccfd1c35bf82
Stefan Worzalla <stefan.worzalla@gmail.com>
2016-08-29 19:23:17 +0700

    implemented swipe between fragments

---
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -0,0 +1,65 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by worzst on 29/08/16.
+ * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Word} objects.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
+    public CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Return the total number of pages.
+     */
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Numbers";
+            case 1:
+                return "Family";
+            case 2:
+                return "Colors";
+            case 3:
+                return "Phrases";
+            default:
+                return null;
+        }
+    }
+}
--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,64 +15,26 @@
  */
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        // Find Views for the different categories and set their onClick methods for the intents
-        TextView numbers = (TextView) findViewById(R.id.numbers);
-        if (numbers != null) {
-            numbers.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this, NumbersActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        TextView colors = (TextView) findViewById(R.id.colors);
-        if (colors != null) {
-            colors.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
 
-        TextView family = (TextView) findViewById(R.id.family);
-        if (family != null) {
-            family.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }
-
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-        if (phrases != null) {
-            phrases.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this, PhrasesActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
     }
-
 }
